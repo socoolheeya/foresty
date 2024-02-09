9f4994389e3e832c2ba837cb4e4cbc5c4c240a25
wonhee.lee <lwh0102@gmail.com>
2024-02-09 22:13:33 +0900

    Fixed: fix setting files

--- foresty-api/build.gradle.kts
+++ foresty-api/build.gradle.kts
@@ -1,9 +1,14 @@
 description="foresty-api"
 
-dependencies {
-    implementation(project(":foresty-domain"))
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
+//dependencies {
+//    api(project(":foresty-domain"))
+//    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+//    implementation("org.springframework.boot:spring-boot-starter-web")
+//    developmentOnly("org.springframework.boot:spring-boot-devtools")
+//}
+//val bootJar: BootJar by tasks
+//bootJar.mainClass.set("com.foresty.api.ForestyApiApplication")
+//
+//tasks.withType<Test> {
+//    useJUnitPlatform()
+//}
