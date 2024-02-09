03fb31f34b30881b5fc986424cb777e4df833e82
wonhee.lee <wonhee.lee@tidesquare.com>
2024-02-09 22:12:21 +0900

    Feat: add DSL entities and DAO entities

---
+++ foresty-domain/src/main/kotlin/com/foresty/domain/entities/note/Notes.kt
@@ -0,0 +1,18 @@
+package com.foresty.domain.entities.note
+
+import com.foresty.domain.entities.user.Users
+import com.foresty.domain.enums.NoteEnum
+import org.jetbrains.exposed.dao.id.LongIdTable
+import org.jetbrains.exposed.sql.Column
+
+
+object Notes: LongIdTable(name = "note") {
+    val title: Column<String?> = varchar(name = "title", length = 255).nullable()
+    val star: Column<NoteEnum.Star?> = customEnumeration(
+        name = "star",
+        sql = "ENUM('STAR1', 'STAR2', 'STAR3', 'STAR4', 'STAR5')",
+        fromDb = { value -> NoteEnum.Star.from(value as String) as NoteEnum.Star },
+        toDb =  { value -> NoteEnum.Star.entries.find{ it == value }?.code as Any }
+    ).nullable()
+    val user = reference("user", Users.id)
+}
---
+++ foresty-domain/src/main/kotlin/com/foresty/domain/entities/user/Users.kt
@@ -0,0 +1,9 @@
+package com.foresty.domain.entities.user
+
+import org.jetbrains.exposed.dao.id.LongIdTable
+
+object Users: LongIdTable(name = "user") {
+    val name = varchar(name = "name", length = 255)
+    val password = varchar(name = "password", length = 500)
+    val email = varchar(name = "email", length = 500)
+}
