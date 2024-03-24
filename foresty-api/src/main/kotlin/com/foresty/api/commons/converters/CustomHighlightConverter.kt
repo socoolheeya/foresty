021ecccfe70b956af2b7784dca2e991beda403f3
wonhee.lee <lwh0102@gmail.com>
2024-03-25 00:21:39 +0900

    :wrench: Feat: logback log highlight converter 추가

---
+++ foresty-api/src/main/kotlin/com/foresty/api/commons/converters/CustomHighlightConverter.kt
@@ -0,0 +1,18 @@
+package com.foresty.api.commons.converters
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.pattern.color.ANSIConstants
+import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase
+
+class CustomHighlightConverter: ForegroundCompositeConverterBase<ILoggingEvent>() {
+    override fun getForegroundColorCode(event: ILoggingEvent): String {
+        return when (event.level.levelInt) {
+            Level.ERROR.toInt() -> ANSIConstants.RED_FG
+            Level.WARN.toInt() -> ANSIConstants.YELLOW_FG
+            Level.INFO.toInt() -> ANSIConstants.BLUE_FG
+            Level.DEBUG.toInt() -> ANSIConstants.GREEN_FG
+            else -> ANSIConstants.CYAN_FG
+        }
+    }
+}
