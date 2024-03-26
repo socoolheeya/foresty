65dca4270720c0b1ae65c7080b550556055c57f3
wonhee.lee <lwh0102@gmail.com>
2024-03-26 20:16:14 +0900

    :sparkles: Feat: querydsl configuration 추가

---
+++ foresty-domain/src/main/kotlin/com/foresty/domain/configuration/QueryDslConfiguration.kt
@@ -0,0 +1,18 @@
+package com.foresty.domain.configuration
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class QueryDslConfiguration(
+    @PersistenceContext val entityManager: EntityManager
+) {
+
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory {
+        return JPAQueryFactory(entityManager)
+    }
+}
